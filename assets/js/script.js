d30ead7ee7c9e6c62e9b358c0bd3924654b6bc32
Tiago2526 <tiagonizferreira@gmail.com>
2024-03-05 11:58:31 +0100

    online and with some js

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+	alert('Website is still under construction!');
+
+});
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    
+    // voer code uit als je op de knop klikt
+    document.getElementById("toonVerhaalAsh").addEventListener("click", function() {
+    
+				// test of de knop werkt
+        console.log('Toon verhaal');
+
+        // verwijder en voeg de .verborgen class toe
+        document.getElementById("ash").classList.remove("ash    ");
+        
+    });
+
+});
